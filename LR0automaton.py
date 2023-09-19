5aae587570ca5706faf6d931cde5dd97c650da3e
Sakshi Yeole <sakshiyeole210@gmail.com>
2023-09-19 23:32:33 +0530

    Added LR0 automaton class

--- LR0automaton.py
+++ LR0automaton.py
@@ -22,6 +22,7 @@ from pathlib import Path
 from enum import Enum
 import shutil
 import subprocess
+from collections import deque
 
 class ProductionRule:           #Tested
 
@@ -93,7 +94,7 @@ class Item:
         indexOfDotMarker = self.RHS.index(self.dotMarker)
         self.RHS.remove(self.dotMarker)
         self.RHS.insert(indexOfDotMarker + 1, self.dotMarker)
-        return Item(self.LHS, newRHS, self.itemType.derived_item)
+        return Item(self.LHS, newRHS, itemType.derived_item)
     
     def closure(self, production_rules):
         closure_item = {self}
@@ -163,37 +164,6 @@ class State:
         for item in self.items:
             stateStr += str(item) + "\n"
         return stateStr
-    
-class DFA:
-
-    def __init__(self) -> None:
-        self.states = []
-        self.listOfStateNo = []
-        self.startState = 0
-        self.acceptingStateNo = None
-
-    def __str__(self) -> str:
-        s = ""
-        for i in self.states:
-            s += i.__str__()
-        return s
-    
-    def getListOfStateNo(self):
-        return self.listOfStateNo
-    
-    def getAcceptingStateNo(self):
-        return self.acceptingStateNo
-    
-    def getStateItemsBasedOnStateNo(self, stateno):
-        state = State(stateno)
-        if state in self.states:
-            index = self.states.index(state)
-            return self.states[index]
-        else:
-            return None
-        
-    def addState(self, input):
-        pass
 
 class Grammar:                   
     epsilon = '\u03B5'
@@ -318,6 +288,105 @@ class Grammar:
                 writer.write(str(productionRule) + "\n")
     
 
+class LR0Grammar(Grammar):
+
+    def __init__(self):
+        super().__init__()
+        self.transitions = {}
+        self.initialState = None
+
+    # def __str__(self) -> str:
+    #     s = ""
+    #     for i in self.states:
+    #         s += i.__str__()
+    #     return s
+
+    def findInitialItem(self):
+        firstSymbol = super().getFirstSymbol()
+        newNameForFirstSymbol = super().findNewName(firstSymbol)
+
+        rightOfItem = [firstSymbol]
+        return Item(newNameForFirstSymbol, rightOfItem, itemType.new_item)
+    
+    def findInitialItemSet(self):
+        initial_Item = self.findInitialItem()
+        InitialItem_set = {initial_Item}
+        return InitialItem_set
+    
+    def addToTransitions(self, from_state, transition_string, to_state):
+        if from_state not in self.transitions:
+            self.transitions[from_state] = {}
+        self.transitions[from_state][transition_string] = to_state
+
+    def checkStateAlreadyServed(self, state):
+        return state in self.transitions
+    
+    def computeTransitions(self):
+        initial_item_Set = self.findInitialItemSet()
+        self.initialState = State(initial_item_Set, super().getProductionRules())
+
+        new_states_created = deque()
+        new_states_created.append(self.initialState)
+
+        while new_states_created:
+            curr_state = new_states_created.popleft()
+
+            # check if the state is already served
+            if self.checkStateAlreadyServed(curr_state):
+                continue
+            string_transition_possible = set()
+            for item in curr_state.getItems():
+                if not item.isReductionItem():
+                    string_transition_possible.add(item.getSymbolNextToDotMarker())
+
+            for transitionString in string_transition_possible:
+                non_closure_items_for_new_state = set()
+                for item in curr_state.getItems():
+                    if not item.isReductionItem() and item.getSymbolNextToDotMarker() == transitionString:
+                        non_closure_items_for_new_state.add(item.movingDotMarkerInItemAndReturn())
+
+                newstate = State(non_closure_items_for_new_state, super().getProductionRules())
+                new_states_created.append(newstate)
+                self.addToTransitions(curr_state, transitionString, newstate)
+
+    def indexingStates(self):
+        result = {}
+        index = 0
+
+        for from_state, transition_map in self.transitions.items():
+            if from_state not in result:
+                result[from_state] = index
+                index += 1
+
+            for to_state in transition_map.values():
+                if to_state not in result:
+                    result[to_state] = index
+                    index += 1
+        
+        print("Total States: ", len(result))
+        print("Indexing of maps: ")
+        for state, index in result.items():
+            print(f"{index} :- ")
+            print(state)
+
+        return result
+    
+    def printTransitions(self):
+        stateIndexing = self.indexingStates()
+
+        for from_state, transition_map in self.transitions.items():
+            from_state_index = stateIndexing[from_state]
+
+            for transition_string, to_state in transition_map.items():
+                to_state_index = stateIndexing[to_state]
+
+                print("From State: ", from_state_index)
+                print("Transition String: ", transition_string)
+                print("To State: ", to_state_index)
+                print()
+        
+        print()
+        
 class ReadingInput:
     def readInputFromFile(path):
         with open(path, 'r') as file:
@@ -346,47 +415,52 @@ def main():
     dotMarker = '\u2022'
 
     # Testing
-    t = Grammar()
-    # t.setFirstSymbol("S")
-    # t.addNonterminalSymbol("S")
-    # t.addNonterminalSymbol("L")
-    # t.addNonterminalSymbol("L'")
-    # t.addTerminalSymbol("(")
-    # t.addTerminalSymbol(")")
-    # t.addTerminalSymbol("a")
+    t = LR0Grammar()
+    t.setFirstSymbol("E")
+    t.addNonterminalSymbol("E")
+    t.addNonterminalSymbol("T")
+    t.addNonterminalSymbol("F")
+    t.addTerminalSymbol("+")
+    t.addTerminalSymbol("*")
+    t.addTerminalSymbol("(")
+    t.addTerminalSymbol(")")
+    t.addTerminalSymbol("F")
     t.addRule("E' -> E")
     t.addRule("E -> E + T | T")
     t.addRule("T -> T * F | F")
     t.addRule("F -> ( E ) | id")
     t.printGrammar()
 
-    right = []
-    right.append("(")
-    right.append(dotMarker)
-    right.append("E")
-    right.append(")")
-    i = Item("F", right, itemType.derived_item)
-    print(f"Item: {i}")
+    # right = []
+    # right.append("(")
+    # right.append(dotMarker)
+    # right.append("E")
+    # right.append(")")
+    # i = Item("F", right, itemType.derived_item)
+    # print(f"Item: {i}")
 
     # closure = set()
     # closure = i.closure(t.getProductionRules())
     # for i in  closure:
     #     print(i)
 
-    inititemset = set()
-    inititemset.add(i)
+    # inititemset = set()
+    # inititemset.add(i)
 
-    s1 = State(inititemset, t.getProductionRules())
-    print(s1)
-    s2 = State(inititemset, t.getProductionRules())
-    print(s2)
+    # s1 = State(inititemset, t.getProductionRules())
+    # print(s1)
+    # s2 = State(inititemset, t.getProductionRules())
+    # print(s2)
 
-    print(s1==s2)
+    # print(s1==s2)
 
-    states = set()
-    states.add(s1)
-    states.add(s2)
-    print(f"states size: {len(states)}")
+    # states = set()
+    # states.add(s1)
+    # states.add(s2)
+    # print(f"states size: {len(states)}")
+
+    t.computeTransitions()
+    t.printTransitions()
 
 if __name__ == "__main__":
     main()
